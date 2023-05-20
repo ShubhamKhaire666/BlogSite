939eb30e4de055d9aff42555780d19e42394cc8e
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-05-21 00:13:33 +0300

    Add project files.

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,39 @@
+﻿using BlogSite.Data;
+using BlogSite.Models.Domain;
+using BlogSite.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogSiteDBContext _blogSiteDBContext;
+        public AdminTagsController(BlogSiteDBContext blogSiteDBContext)
+        {
+            this._blogSiteDBContext = blogSiteDBContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+
+            Tag tag = new Tag()
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            _blogSiteDBContext.Tags.Add(tag);
+            _blogSiteDBContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ Data/BlogSiteDBContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogSite.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Data
+{
+    public class BlogSiteDBContext: DbContext
+    {
+        public BlogSiteDBContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
